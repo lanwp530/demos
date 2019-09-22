efcb6772a21ce63ee3e6ca1c5ba880ece8305be9
lanwp <lanwp530@163.com>
2019-09-22 12:00:31 +0800

    swagger2 demo 授权及swagger2注解

---
+++ swagger2-auth-demo/src/main/java/com/example/lwp/swagger2/swagger2authdemo/Swagger2AuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lwp.swagger2.swagger2authdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2AuthDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger2AuthDemoApplication.class, args);
+    }
+
+}
